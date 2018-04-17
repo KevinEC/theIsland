a465d8d2454d7e8bfdb1f62b55b674f3632f9055
Kevin Evans <kevev243@student.liu.se>
2018-04-17 13:49:54 +0200

    fixed multiple var inits

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ function init() {
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 	camera.position.z = 5;
 	
-	var scene = new THREE.Scene();
+	scene = new THREE.Scene();
 	
-	var renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer();
 	renderer.setClearColor( 0x340000 );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
